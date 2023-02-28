066b029d5baf6c17f04374b34867307fba791945
JM <92850744+BlaSee01@users.noreply.github.com>
2023-02-28 18:13:08 +0100

    Add files via upload

---
+++ Banały (basis)/5. Algorytm sprawdzający czy liczba jest liczbą pierwszą/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int a;
+    int dzielniki = 0;
+    cout << "Podaj liczbê: ";
+    cin >> a;
+
+    for(int i = 2; i <= a ; i++){
+        if(a%i==0)  dzielniki++;
+    }
+
+    if(dzielniki > 2){
+        cout << "Nie jest liczb¹ pierwsz¹!";
+    }else{
+        cout << "Jest liczb¹ peirwsz¹!";
+    }
+
+    return 0;
+}
